f8564c9332a2549d3e3a1e2999c92136475d1510
Adam Swinford <aswinford@jadam.space>
2025-06-26 23:38:22 -0400

    Initial data analysis

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath):
+    output = None
+    with open(filepath) as f:
+        output = f.read()
+    return output
+
+def get_wordcount(body):
+    words = body.split()
+    return len(words)
+
+def main():
+    frankenstein = get_book_text("books/frankenstein.txt")
+    frankenstein_wordcount = get_wordcount(frankenstein)
+    print(f"{frankenstein_wordcount} words found in the document")
+
+main()
